ff565d193b5bc048dc452decfbc05ea0e5fbf350
sindetisarah <sindetisara@gmail.com>
2021-02-28 11:06:41 +0300

    made some changes

--- src/Functions.kt
+++ src/Functions.kt
@@ -14,17 +14,20 @@ fun name(){
     val name = ("Sarah")
     println("Hello ${name}")
 }
+
 fun module(a:Int,b:Int):Int{
 
     var module = a % b
     return module
 
 }
+
 fun add(a:Int,b:Int,c:Int,d:Int):Int{
     var sum = a + b + d+ c
     return sum
 
 }
+
 fun interest(){
     var fact =("I cant help laughing when i feel like.")
     println(fact)
