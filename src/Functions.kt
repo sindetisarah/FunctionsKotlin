3fbb99314a79a4078836e8c9a353699dfab04ab7
sindetisarah <sindetisara@gmail.com>
2021-02-28 09:42:14 +0300

    created three functions module,interest and name and called them to the main function

---
+++ src/Functions.kt
@@ -0,0 +1,21 @@
+fun main() {
+name()
+    module()
+    interest()
+}
+
+fun name(){
+    var name = ("Sarah")
+    println("Hello ${name}")
+}
+fun module(){
+    var a = 20
+    var b = 8
+    var module = a % b
+    println(module)
+
+}
+fun interest(){
+    var fact =("I cant help laughing when i feel like.")
+    println(fact)
+}
